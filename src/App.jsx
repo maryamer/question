e6223da144a4145f0846edd0cc1be018ba313ecb
maryam <personalmaryamebrahimi@gmail.com>
2024-08-06 00:14:58 +0330

    secound commit

--- src/App.jsx
+++ src/App.jsx
@@ -104,7 +104,6 @@ export default function App() {
             isFavourite={isFavourite}
           />
         )}
-        {/* <CharracterDetail selectedCharacter={selectedCharacter} /> */}
       </div>
     </div>
   );
